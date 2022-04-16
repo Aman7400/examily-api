75535b388d45aa0785acc5e1dd8a7eb64e95e63a
Aman Shukla <amanks7400@gmail.com>
2022-04-16 10:36:46 +0530

    Add Create Exam Validation

--- controllers/exams.js
+++ controllers/exams.js
@@ -10,12 +10,63 @@ const postCreateNewExam = async (req, res, next) => {
       throw new Error('Unauthorized');
     }
     const { examObject } = req.body;
-    //   * Validations
-    //   TODO Validations
-    //   * Create a new exam
+
+    // * Validations
+    const schema = Joi.object({
+      name: Joi.string().required().messages({
+        'any.required': 'Exam Name is required',
+      }),
+      expireIn: Joi.number().required().messages({
+        'any.required': 'Exam Expiry is required',
+      }),
+      status: Joi.string().required().messages({
+        'any.required': 'Exam Status is required',
+      }),
+      questions: Joi.array()
+        .required()
+        .min(1)
+        .items(
+          Joi.object({
+            title: Joi.string().required().messages({
+              'any.required': 'Question Title is required',
+            }),
+            correctAnswer: Joi.number().required().messages({
+              'any.required': 'Correct Answer is required',
+            }),
+            options: Joi.array()
+              .length(4)
+              .items(
+                Joi.string().messages({
+                  'string.base': 'All Options must be a string',
+                })
+              )
+              .messages({
+                'array.length': 'Only 4 options allowed',
+              }),
+          })
+        )
+        .messages({
+          'any.required': 'Exam Questions are required',
+          'array.min': 'min 1 question is required',
+        }),
+    });
+
+    const { error } = schema.validate({
+      ...examObject,
+    });
+
+    if (error) {
+      res.status(400);
+      throw new Error(error.details[0].message);
+    }
+
+    //  * Generate Exam Key
     const answerKey = examObject.questions.map(
       (question) => question.correctAnswer
     );
+
+    //  * Create a new exam
+
     const newExam = new Exam({
       ...examObject,
       createdBy: req.user._id,
@@ -24,6 +75,7 @@ const postCreateNewExam = async (req, res, next) => {
     await newExam.save();
     res.json({ message: 'Exam created successfully', exam: newExam._id });
   } catch (error) {
+    console.log(error);
     next(error);
   }
 };
